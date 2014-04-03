df97f4b62ba8f5d435a7058efb65b9602db3c18e
ypw1380 <ypw1380@gmail.com>
2014-04-03 02:30:01 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the site!"
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem creating your account. Please try again."
+      redirect_to :back
+    end
+    
+    private
+    
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workshop::Application.config.secret_key_base = 'ba37db2d6a265bba35a6ef92716e3b3fc3e8ae14c1a33d466a321172e4c00e5953ae75a80852e9fb812216f44bc03ca60f131c6df64984ec19e30cd4aeb6930c'
